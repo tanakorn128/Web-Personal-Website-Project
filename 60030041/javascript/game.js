36ad694859c8e64f9582ba9eb2dd91298858665d
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-05-12 18:54:34 +0700

    update

--- 60030041/javascript/game.js
+++ 60030041/javascript/game.js
@@ -1,45 +1,49 @@
+//create canvas id games
 var canvas = document.getElementById(`games`);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+//context canvas 
 var context = canvas.getContext('2d');
 
-var  posX = Math.random() * window.innerWidth,
-     posY = Math.random() * window.innerWidth,
-     playX = Math.random() * window.innerWidth,
-     playY = window.innerHeight - 100,
-     speedX = 30,
-     speedY = 30,
-     speedXb = 5,
-     speedYb = 5,
-     areaX = window.innerWidth;
-     areaY = window.innerHeight;
-     wallY = window.innerHeight - 400,
+var  posX = Math.random() * window.innerWidth, //ลูกบอลตำหน่งสุ่ม dX
+     posY = Math.random() * window.innerWidth, //ลูกบอลตำหน่งสุ่ม Y
+     playX = Math.random() * window.innerWidth, //ตำแหน่งของผู้เล่นด้าน X ถูกสุ่ม แต่ไม่เกินความหว้างหน้าจอ
+     playY = window.innerHeight - 100, //ตำแน่งผู้เล่นด้าน Y กำหนดตายตัว
+     speedX = 30, //ความเร็วผู้เล่นX
+     speedY = 30, //ความเร็วผู้เล่นY
+     speedXb = 5, //ความเร็วบอลX
+     speedYb = 5,//ความเร็วบอลY
+     areaX = window.innerWidth; //ความกว่างของไหน้าจอ X
+     areaY = window.innerHeight; //ความกว่างของไหน้าจอ Y
+     wallY = window.innerHeight - 400, //ความสูงขีดกั้น
      score = 0;
      
      
 function player(){
+    `use strict`
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    context.fillRect(playX, playY, 100, 10);  //create rectangle
+    context.fillRect(playX, playY, 100, 10);  //create rectangle ball
     context.fillRect(0, wallY + 100, areaX, 10);  //create wall
     context.fillRect(posX, posY, 30, 30);
    
-    window.requestAnimationFrame(player);
+    window.requestAnimationFrame(player); //requrst ตลอดเวลา
     speedball();
 }
 player();
 
-function area(){
-    if(playX < 0){
+function area(){ //กำหนดขอบเขต ผู้เล่น
+    `use strict`
+    if(playX < 0){ //ถ้าชิดซ้ายสุดจอ ให้ position playX เป็น 0
         playX = 0;
     }
-    if(playX > window.innerWidth - 100){
+    if(playX > window.innerWidth - 100){ //ถ้าชิดขวามากไปให้ให้หยุดทที่ตำแหน้ง ขวาสุด-100 
         playX = window.innerWidth - 100;
     }
    
 }
 function speedball(){
+    `use strict`
     var showscore = `Score = ` + score;
     if(posX > window.innerWidth - 30|| posX < 0 || posX == playX){
         speedXb = -speedXb;
@@ -47,7 +51,7 @@ function speedball(){
     if(posY > window.innerHeight - 30|| posY < 0){
         speedYb = -speedYb;
     }
-    if(posY > playY && posY < playY + 3 && posX > playX && posX < playX + 100){
+    if(posY > playY && posY < playY + 3 && posX > playX && posX < playX + 100){//เช็กว่าบอลชนกับผู้เล่นหรือไม่
         console.log(`true`);
         speedYb = -speedYb;
         score = score + 1;
@@ -56,41 +60,46 @@ function speedball(){
     posX = posX + speedXb;
     
     context.fillText(showscore , 10, 50)
-    context.font = 30 + "px arial";
+    context.font = 30 + "px arial"; //fontของ score 
     
 }
 function moveLeft(){ //moveLeft
+    `use strict`
  playX = playX - speedX; //change positionX
  area();
  console.log(playX);
 
 }
 function moveRight(){ // movereght
+    `use strict`
  playX = playX + speedX; //change positionX
  area();
 }
 function moveUp(){ //moveup
+    `use strict`
 playY = playY - speedY; //change positionY
 area();
 }
 function moveDown(){ //movedown
+    `use strict`
     playY = playY + speedY; //change positionY
     area();
 }
 function keyDownHandler(key){
-if(key.keyCode == 37){
+    `use strict`
+if(key.keyCode == 37){ //mouseLeft
     moveLeft();
 }
-if(key.keyCode == 39){
+if(key.keyCode == 39){ //mouseRight
     moveRight();
 }
-if(key.keyCode == 40){
+if(key.keyCode == 40){ //mouseDown
   //  moveDown();
 }
-if(key.keyCode == 38){
+if(key.keyCode == 38){ //moueUp
    // moveUp();
 }
 }
 
-window.addEventListener(`keydown`,keyDownHandler);
+window.addEventListener(`keydown`,keyDownHandler); 
 
